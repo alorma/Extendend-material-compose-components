25a2ff7c2857d9f4a78e02fa0adbcaf6c2c9b6e0
Bernat Borrás <bernat.borras@adevinta.com>
2021-07-20 16:21:08 +0200

    Make buttons have same params as their underlayer component

--- components/src/main/java/com/alorma/components/buttons/Buttons.kt
+++ components/src/main/java/com/alorma/components/buttons/Buttons.kt
@@ -1,154 +1,250 @@
 package com.alorma.components.buttons
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.ButtonElevation
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 
 @Composable
 fun PrimaryButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     Button(
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun SecondaryButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     Button(
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun ErrorButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.elevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     Button(
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun PrimaryOutlinedButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = ButtonDefaults.outlinedBorder,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     OutlinedButton(
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.primary),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.primary),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun SecondaryOutlinedButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = ButtonDefaults.outlinedBorder,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     OutlinedButton(
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.secondary),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.secondary),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun ErrorOutlinedButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = ButtonDefaults.outlinedBorder,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     OutlinedButton(
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.error),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.error),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun PrimaryTextButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     TextButton(
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.primary),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.primary),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun SecondaryTextButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     TextButton(
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.secondary),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.secondary),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
 
 @Composable
 fun ErrorTextButton(
+    onClick: () -> Unit,
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
-    onClick: () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = null,
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.TextButtonContentPadding,
     content: @Composable RowScope.() -> Unit,
 ) {
     TextButton(
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.error),
+        onClick = onClick,
         modifier = modifier,
         enabled = enabled,
-        onClick = onClick,
-        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.error),
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        contentPadding = contentPadding,
         content = content,
     )
 }
