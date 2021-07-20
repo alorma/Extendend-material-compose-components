f6fc88ced25ca2d6938298039c1a218ed430755d
Bernat Borrás <bernat.borras@adevinta.com>
2021-07-20 16:06:51 +0200

    Init commit

--- app/src/main/java/com/alorma/components/MainActivity.kt
+++ app/src/main/java/com/alorma/components/MainActivity.kt
@@ -3,11 +3,26 @@ package com.alorma.components
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.alorma.components.buttons.ErrorButton
+import com.alorma.components.buttons.ErrorOutlinedButton
+import com.alorma.components.buttons.ErrorTextButton
+import com.alorma.components.buttons.PrimaryButton
+import com.alorma.components.buttons.PrimaryOutlinedButton
+import com.alorma.components.buttons.PrimaryTextButton
+import com.alorma.components.buttons.SecondaryButton
+import com.alorma.components.buttons.SecondaryOutlinedButton
+import com.alorma.components.buttons.SecondaryTextButton
 import com.alorma.components.ui.theme.ExtendedMaterialComposecomponentsTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,9 +30,16 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ExtendedMaterialComposecomponentsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+                Column(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    FilledButtons()
+                    Spacer(modifier = Modifier.height(16.dp))
+                    OutlinedButtons()
+                    Spacer(modifier = Modifier.height(16.dp))
+                    TextButtons()
                 }
             }
         }
@@ -25,14 +47,97 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun FilledButtons() {
+    Row {
+        PrimaryButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Primary")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        SecondaryButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Secondary")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        ErrorButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Error")
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ExtendedMaterialComposecomponentsTheme {
-        Greeting("Android")
+fun OutlinedButtons() {
+    Row {
+        PrimaryOutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Primary")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        SecondaryOutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Secondary")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        ErrorOutlinedButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Error")
+        }
+    }
+}
+
+@Composable
+fun TextButtons() {
+    Row {
+        PrimaryTextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Primary")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        SecondaryTextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Secondary")
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+        ErrorTextButton(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            onClick = {},
+        ) {
+            Text(text = "Error")
+        }
     }
 }
--- components/src/main/java/com/alorma/components/buttons/Buttons.kt
+++ components/src/main/java/com/alorma/components/buttons/Buttons.kt
@@ -1,2 +1,136 @@
 package com.alorma.components.buttons
 
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PrimaryButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.primary),
+        content = content,
+    )
+}
+
+@Composable
+fun SecondaryButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.secondary),
+        content = content,
+    )
+}
+
+@Composable
+fun ErrorButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(backgroundColor = MaterialTheme.colors.error),
+        content = content,
+    )
+}
+
+@Composable
+fun PrimaryOutlinedButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.primary),
+        content = content,
+    )
+}
+
+@Composable
+fun SecondaryOutlinedButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.secondary),
+        content = content,
+    )
+}
+
+@Composable
+fun ErrorOutlinedButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.outlinedButtonColors(contentColor = MaterialTheme.colors.error),
+        content = content,
+    )
+}
+
+@Composable
+fun PrimaryTextButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.primary),
+        content = content,
+    )
+}
+
+@Composable
+fun SecondaryTextButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.secondary),
+        content = content,
+    )
+}
+
+@Composable
+fun ErrorTextButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    content: @Composable RowScope.() -> Unit,
+) {
+    TextButton(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colors.error),
+        content = content,
+    )
+}
