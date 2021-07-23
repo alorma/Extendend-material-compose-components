894b336c643abefc745435e7c88fe414fe3cb1fa
Bernat Borrás <bernat.borras@adevinta.com>
2021-07-23 10:38:46 +0200

    Banner complete

--- app/src/main/java/com/alorma/components/MainActivity.kt
+++ app/src/main/java/com/alorma/components/MainActivity.kt
@@ -3,9 +3,14 @@ package com.alorma.components
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,27 +19,41 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Wifi
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.unit.dp
+import com.alorma.components.buttons.ActionItem
 import com.alorma.components.buttons.Banner
+import com.alorma.components.buttons.TopAppBar
 import com.alorma.components.samples.ButtonColorsSample
 import com.alorma.components.samples.CardColorsSample
-import com.alorma.components.samples.SampleTopAppBar
 import com.alorma.components.ui.theme.ExtendedMaterialComposecomponentsTheme
-import kotlinx.coroutines.launch
 
+@ExperimentalAnimationApi
 class MainActivity : ComponentActivity() {
 
     @OptIn(ExperimentalFoundationApi::class)
@@ -42,96 +61,146 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ExtendedMaterialComposecomponentsTheme {
+                var bannerState by remember { mutableStateOf<BannerMessage?>(null) }
+                val bannerVisibleState by derivedStateOf { bannerState != null }
                 val scaffoldState = rememberScaffoldState()
 
                 Scaffold(
                     scaffoldState = scaffoldState,
-                    topBar = { DemoAppBar(scaffoldState) }
+                    topBar = {
+                        DemoAppBar(
+                            action = ActionItem(
+                                id = 0,
+                                content = { Text(text = "Show banner") },
+                                icon = Icons.Default.Info,
+                                onClick = {
+                                    bannerState = fakeBanner {
+                                        bannerState = null
+                                    }.takeIf { bannerState == null }
+                                    true
+                                },
+                            )
+                        )
+                    }
                 ) {
                     Column {
-                        OnLineBanner()
-                        Spacer(modifier = Modifier.size(64.dp))
-                        TwoLineBanner()
-                        Spacer(modifier = Modifier.size(120.dp))
-                        TwoLineSingleButtonBanner()
+                        AnimatedVisibility(visible = bannerVisibleState) {
+                            bannerState?.let {
+                                TwoLineBanner(it)
+                            }
+                        }
+                        LazyColumn {
+                            items(10) { num -> ItemCard(num) }
+                        }
                     }
                 }
             }
         }
     }
-
 }
 
+private fun fakeBanner(
+    onClick: () -> Unit,
+): BannerMessage = BannerMessage(
+    text = "Your internet connection was lost.\nWe can't upload your photo.",
+    firstButton = BannerButton(
+        text = "LEARN MORE",
+        onClick = onClick,
+    ),
+    secondButton = BannerButton(
+        text = "FIX IT",
+        onClick = onClick,
+    )
+)
+
+data class BannerMessage(
+    val text: String,
+    val firstButton: BannerButton,
+    val secondButton: BannerButton? = null,
+)
+
+data class BannerButton(
+    val text: String,
+    val onClick: () -> Unit,
+)
 
 @Composable
-fun OnLineBanner() {
-    Banner(
-        buttons = {
-            TextButton(
-                onClick = { /*TODO*/ },
-            ) {
-                Text(text = "Button 1")
-            }
-            TextButton(
-                onClick = { /*TODO*/ },
-            ) {
-                Text(text = "Button 2")
-            }
-        }
+fun ItemCard(num: Int) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
     ) {
-        Text(text = "Banner content")
+        Column {
+            Image(
+                painter = ColorPainter(color = MaterialTheme.colors.background.copy(alpha = 0.12f)),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp),
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+            ) {
+                Text(text = "Item #$num")
+            }
+
+        }
     }
 }
 
 @Composable
-fun TwoLineBanner() {
+fun TwoLineBanner(bannerState: BannerMessage) {
     Banner(
-        buttons = {
+        firstButton = {
             TextButton(
-                onClick = { /*TODO*/ },
+                onClick = bannerState.firstButton.onClick,
             ) {
-                Text(text = "Button 1")
+                Text(text = bannerState.firstButton.text)
             }
-            TextButton(
-                onClick = { /*TODO*/ },
-            ) {
-                Text(text = "Button 2")
+        },
+        secondButton = bannerState.secondButton?.let {
+            @Composable {
+                TextButton(
+                    onClick = bannerState.secondButton.onClick,
+                ) {
+                    Text(text = bannerState.secondButton.text)
+                }
             }
-        }
+        },
     ) {
-        Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer justo ligula, lacinia vitae massa vel, bibendum porttitor tortor.")
-    }
-}
-
-@Composable
-fun TwoLineSingleButtonBanner() {
-    Banner(
-        buttons = {
-            TextButton(
-                onClick = { /*TODO*/ },
-            ) {
-                Text(text = "Button 1")
-            }
+        Row(
+            modifier = Modifier
+                .padding(horizontal = 12.dp)
+                .padding(top = 8.dp),
+            verticalAlignment = Alignment.Top
+        ) {
+            Icon(
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .background(color = MaterialTheme.colors.primary)
+                    .size(48.dp)
+                    .padding(8.dp),
+                imageVector = Icons.Default.Wifi,
+                contentDescription = null,
+                tint = MaterialTheme.colors.onPrimary,
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(text = bannerState.text)
         }
-    ) {
-        Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer justo ligula, lacinia vitae massa vel, bibendum porttitor tortor.")
     }
 }
 
 @Composable
 fun DemoAppBar(
-    scaffoldState: ScaffoldState,
+    action: ActionItem,
 ) {
-    val coroutineScope = rememberCoroutineScope()
-    SampleTopAppBar(
-        onActionClick = { action ->
-            coroutineScope.launch {
-                scaffoldState.snackbarHostState.showSnackbar(
-                    message = "Clicked: ${action.contentDescription}"
-                )
-            }
-            true
-        }
+    TopAppBar(
+        title = { Text(text = "Sample") },
+        actions = listOf(action),
     )
 }
 
--- components/src/main/java/com/alorma/components/buttons/Banner.kt
+++ components/src/main/java/com/alorma/components/buttons/Banner.kt
@@ -1,13 +1,86 @@
 package com.alorma.components.buttons
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ProvideTextStyle
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Banner(
     modifier: Modifier = Modifier,
-    buttons: @Composable () -> Unit,
+    firstButton: @Composable () -> Unit,
+    secondButton: (@Composable () -> Unit)? = null,
+    divider: @Composable () -> Unit = { Divider(modifier = Modifier.fillMaxWidth()) },
     content: @Composable () -> Unit,
 ) {
+    Layout(
+        modifier = modifier,
+        content = {
+            Box(
+                modifier = Modifier.padding(horizontal = 8.dp, vertical = 8.dp),
+                contentAlignment = Alignment.CenterStart,
+            ) {
+                ProvideTextStyle(value = MaterialTheme.typography.body2) {
+                    content()
+                }
+            }
+            if (secondButton != null) {
+                Row(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    firstButton()
+                    Spacer(modifier = Modifier.size(8.dp))
+                    secondButton()
+                }
+            } else {
+                Box(modifier = Modifier.padding(horizontal = 8.dp)) {
+                    firstButton()
+                }
+            }
+            divider()
+        },
+    ) { measurable, constrains ->
 
+        val noMinConstrains = constrains.copy(
+            minWidth = 0,
+            minHeight = 0,
+        )
+
+        val contentMeasurable = measurable[0]
+        val buttonsMeasurable = measurable[1]
+        val dividerMeasurable = measurable[2]
+
+        val contentPlaceable = contentMeasurable.measure(noMinConstrains)
+        val buttonsPlaceable = buttonsMeasurable.measure(noMinConstrains)
+        val dividerPlaceable = dividerMeasurable.measure(noMinConstrains)
+
+        var buttonsYOffset = 0
+        val buttonsXOffset = constrains.maxWidth - buttonsPlaceable.width
+
+        var boxHeight = maxOf(
+            contentPlaceable.height,
+            buttonsPlaceable.height,
+        ) + dividerPlaceable.height
+
+        if (contentPlaceable.width > constrains.maxWidth - buttonsXOffset) {
+            buttonsYOffset = 8.dp.roundToPx() + contentPlaceable.height
+            boxHeight = buttonsYOffset + buttonsPlaceable.height + 8.dp.roundToPx()
+        }
+
+        val dividerYOffset = boxHeight - dividerPlaceable.height
+
+        layout(constrains.maxWidth, boxHeight) {
+            contentPlaceable.place(0, 0)
+            buttonsPlaceable.place(buttonsXOffset, buttonsYOffset)
+            dividerPlaceable.place(0, dividerYOffset)
+        }
+    }
 }
